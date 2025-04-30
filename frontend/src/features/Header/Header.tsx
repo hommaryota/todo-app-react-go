5fafc3cfcfcce83e647e579b11ab729bd72a117a
hommaryota <ryo4111016@gmail.com>
2025-04-30 23:55:46 +0900

    あばたーで遊ぶ

--- frontend/src/features/Header/Header.tsx
+++ frontend/src/features/Header/Header.tsx
@@ -1,13 +1,33 @@
 import Avatar from "@mui/material/Avatar";
 import s from "./Header.module.css";
-import { IconButton } from "@mui/material";
+import { IconButton, Slider } from "@mui/material";
+import { useState } from "react";
 
 const Header = () => {
+  const [size, setSize] = useState(50);
+
+  const onChangeSlider = (e: any) => {
+    console.log(e);
+
+    setSize(e.target.value);
+  };
   return (
     <header className={s.header}>
       <IconButton>
-        <Avatar alt="Cindy Baker" src="/src/assets/Irasutoya.png" />
+        <Avatar
+          alt="Cindy Baker"
+          src="/src/assets/Irasutoya.png"
+          sx={{ width: size, height: size }}
+        />
       </IconButton>
+      <div>
+        <Slider
+          defaultValue={50}
+          aria-label="Default"
+          valueLabelDisplay="auto"
+          onChange={onChangeSlider}
+        />
+      </div>
     </header>
   );
 };
