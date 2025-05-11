2363190c6dfe94524555b072ee14a631cb7fe055
hommaryota <ryo4111016@gmail.com>
2025-05-11 21:50:10 +0900

    修正

--- backend/middleware/cors.go
+++ backend/middleware/cors.go
@@ -6,7 +6,19 @@ import "net/http"
 func Cors(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		// CORSヘッダーを設定
-		w.Header().Set("Access-Control-Allow-Origin", "http://localhost:3000")
+		allowedOrigins := map[string]bool{
+			"http://localhost:3000": true,
+			"http://localhost:5173": true,
+		}
+
+		// リクエスト元のオリジンを取得
+		origin := r.Header.Get("Origin")
+
+		// CORSヘッダーを設定
+		// リクエスト元のオリジンが許可リストに含まれていれば、そのオリジンを許可
+		if allowedOrigins[origin] {
+			w.Header().Set("Access-Control-Allow-Origin", origin)
+		}
 		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, PATCH, OPTIONS")
 		w.Header().Set("Access-Control-Allow-Headers", "Origin, Content-Type, Content-Length, Accept-Encoding, Authorization")
 
